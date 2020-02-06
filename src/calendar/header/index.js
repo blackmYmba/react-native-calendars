5a92ba861cec2cdd7bbf46ef36923169ed092eb0
Victor <31273424+blackmYmba@users.noreply.github.com>
2020-02-06 19:51:24 +0700

    Update index.js

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -143,16 +143,6 @@ class CalendarHeader extends Component {
 
     return (
       <View style={this.props.style}>
-        <View style={this.style.header}>
-          {leftArrow}
-          <View style={{ flexDirection: 'row' }}>
-            <Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>
-              {this.props.month.toString(this.props.monthFormat)}
-            </Text>
-            {indicator}
-          </View>
-          {rightArrow}
-        </View>
         {
           !this.props.hideDayNames &&
           <View style={this.style.week}>
